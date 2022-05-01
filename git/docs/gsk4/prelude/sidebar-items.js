782b8e29fe640acfe383ae53375ca4a8c7443029
CraigMcDonaldCodes <CraigMcDonaldCodes@users.noreply.github.com>
2022-05-01 11:41:04 +0000

    deploy: bebb1050b58ff9a4284b6358a1bef2ba614fbfa3

--- git/docs/gsk4/prelude/sidebar-items.js
+++ git/docs/gsk4/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["RendererExt","Trait containing all [`struct@Renderer`] methods."],["RendererExtManual",""]]});
+initSidebarItems({"trait":[["GskRendererExt","Trait containing all [`struct@Renderer`] methods."],["IsRenderNode","A common trait implemented by the various `RenderNode` types."]]});
